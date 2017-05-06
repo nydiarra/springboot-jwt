de33b8f9c6f6112966d8a9d55cb6e8b198539df1
Nouhoun Y. Diarra <nydiarra@gmail.com>
2017-05-05 22:36:01 -0400

    Initial project commit

---
+++ src/main/java/com/nouhoun/springboot/jwt/integration/SpringbootJwtApplication.java
@@ -0,0 +1,12 @@
+package com.nouhoun.springboot.jwt.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJwtApplication.class, args);
+	}
+}
